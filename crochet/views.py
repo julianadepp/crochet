16f9943317edc02c92525461d1197255cdde8635
julianadepp <julianadepp7@gmail.com>
2022-01-15 14:03:05 -0500

    image field working, hook model built and corresponding list view with template

--- crochet/views.py
+++ crochet/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Hook
 # Create your views here.
+def hook_list(req):
+    hooks = Hook.objects.all()
+    return render(req, 'crochet/hook_list.html', {'hooks': hooks})
--- crochet_project/settings.py
+++ crochet_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crochet',
+    'imagekit',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Base url to serve media files
+MEDIA_URL = '/images/'
+# Path where media is stored
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
