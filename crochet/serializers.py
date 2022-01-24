3fd1de253d48104c154f593a43a52abbbd553724
julianadepp <julianadepp7@gmail.com>
2022-01-24 09:36:55 -0500

    minr changes for front end, writable serializers for related tables

--- crochet/serializers.py
+++ crochet/serializers.py
@@ -5,12 +5,13 @@ class HookSerializer(serializers.HyperlinkedModelSerializer):
     yarns = serializers.HyperlinkedRelatedField(
         view_name='yarn_info',
         many=True,
-        read_only=True
+        queryset=Hook.objects.all()
     )
     size_name = serializers.CharField(source='get_size_display', read_only=True)
+    yarn_ids = serializers.PrimaryKeyRelatedField(source='yarns', many=True, queryset=Yarn.objects.all())
     class Meta:
         model = Hook
-        fields = ('id', 'size', 'hook_image', 'yarns', 'size_name')
+        fields = ('id', 'size', 'hook_image', 'yarns', 'yarn_ids', 'size_name')
 
 
 class YarnSerializer(serializers.HyperlinkedModelSerializer):
@@ -18,35 +19,35 @@ class YarnSerializer(serializers.HyperlinkedModelSerializer):
         many=True,
         read_only=True
     )
+    hooks = serializers.PrimaryKeyRelatedField(source='suggested_hooks', many=True, queryset=Hook.objects.all())
     class Meta:
         model = Yarn
-        fields = ('id', 'suggested_hooks', 'nickname', 'weight_description', 'brand', 'material', 'weight', 'notes', 'yarn_image', )
+        fields = ('id', 'suggested_hooks', 'hooks', 'nickname', 'weight_description', 'brand', 'material', 'weight', 'notes', 'yarn_image', )
 
 
 class StitchSerializer(serializers.HyperlinkedModelSerializer):
-    related_stitches = serializers.HyperlinkedRelatedField(
-        view_name='stitch_info',
-        many=True,
-        read_only=True
-    )
+    related_stitches = serializers.PrimaryKeyRelatedField( many=True, queryset=Stitch.objects.all())
     class Meta:
         model = Stitch
         fields = ('id', 'name', 'description', 'pattern_code', 'instructions', 'notes', 'related_stitches', 'stitch_image')
 
 class GaugeSerializer(serializers.HyperlinkedModelSerializer):
+    stitch_id = serializers.PrimaryKeyRelatedField(source='stitch', queryset=Stitch.objects.all())
     stitch = StitchSerializer(
         read_only=True
     )
+    hook_id = serializers.PrimaryKeyRelatedField(source='hook', queryset=Hook.objects.all())
     hook = HookSerializer(
         read_only=True
     )
+    yarn_id = serializers.PrimaryKeyRelatedField(source='yarn', queryset=Yarn.objects.all())
     yarn = YarnSerializer(
         read_only=True
     )
     
     class Meta:
         model = Gauge
-        fields = ('id', 'title', 'hook', 'stitch', 'yarn', 'number_of_stitches', 'notes', 'gauge_image')
+        fields = ('id', 'title', 'hook', 'stitch', 'yarn', 'yarn_id', 'hook_id', 'stitch_id', 'number_of_stitches', 'notes', 'gauge_image')
 
 class PatternSerializer(serializers.HyperlinkedModelSerializer):
     stitches = StitchSerializer(
@@ -66,4 +67,4 @@ class PatternSerializer(serializers.HyperlinkedModelSerializer):
     
     class Meta:
         model = Pattern
-        fields = ('id', 'name', 'hook', 'stitches', 'yarn', 'gauge', 'description', 'instructions', 'notes', 'pattern_image')
+        fields = ('id', 'name', 'hook', 'stitches', 'yarn', 'yarn_id', 'hook_id', 'stitch_id', 'gauge', 'description', 'instructions', 'notes', 'pattern_image')
